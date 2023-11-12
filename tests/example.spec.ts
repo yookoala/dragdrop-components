475e4df12765bbf82c2cab65a75be0640a8446f5
Koala Yeung <koalay@gmail.com>
2023-11-12 21:00:58 +0800

    test: add dragdrop test for nested container

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -27,6 +27,7 @@ describe('Simple drag drop', () => {
   test.describe.configure({
     retries: 2,
   });
+
   test('grab "Child 1" to the last container', async ({ page }, testInfo) => {
     let step = 0;
 
@@ -34,6 +35,37 @@ describe('Simple drag drop', () => {
     // Check that the source has the text "Child 1" and the target doesn't.
     const source = page.locator('.grid > dragdrop-container').first();
     const target = page.locator('.grid > dragdrop-container').last();
+    await expect(source).toContainText('Child 1');
+    await expect(target).not.toContainText('Child 1');
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
+
+    // Drag source to target.
+    const child = page.locator('dragdrop-child').nth(0);
+    expect(child).not.toBeNull();
+    await child.hover();
+    await page.mouse.down();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-hover.png` });
+
+    // Drag to outside of the container.
+    await page.mouse.move(400, 400);
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drag.png` });
+
+    // Drop on the target.
+    // Check the drop is successful.
+    await target.hover();
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop.png` });
+    await expect(source).not.toContainText('Child 1');
+    await expect(target).toContainText('Child 1');
+  });
+
+  test('grab "Child 1" to the nested container', async ({ page }, testInfo) => {
+    let step = 0;
+
+    // Identify the source and target containers.
+    // Check that the source has the text "Child 1" and the target doesn't.
+    const source = page.locator('.grid > dragdrop-container').first();
+    const target = page.locator('.grid > dragdrop-container > dragdrop-child > dragdrop-container').first();
     await expect(source).toContainText('Child 1');
     await expect(target).not.toContainText('Child 1');
     await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
