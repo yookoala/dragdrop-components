612ac172724d6858279724a025cd182e141ef12c
Koala Yeung <koalay@gmail.com>
2023-11-12 23:01:54 +0800

    test: rename variable "target"
    * Rename to "destination" to better fit it's purpose.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -34,9 +34,9 @@ describe('Simple drag drop', () => {
     // Identify the source and target containers.
     // Check that the source has the text "Child 1" and the target doesn't.
     const source = page.locator('.grid > dragdrop-container').first();
-    const target = page.locator('.grid > dragdrop-container').last();
+    const destination = page.locator('.grid > dragdrop-container').last();
     await expect(source).toContainText('Child 1');
-    await expect(target).not.toContainText('Child 1');
+    await expect(destination).not.toContainText('Child 1');
     await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
 
     // Drag source to target.
@@ -52,11 +52,11 @@ describe('Simple drag drop', () => {
 
     // Drop on the target.
     // Check the drop is successful.
-    await target.hover();
+    await destination.hover();
     await page.mouse.up();
     await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop.png` });
     await expect(source).not.toContainText('Child 1');
-    await expect(target).toContainText('Child 1');
+    await expect(destination).toContainText('Child 1');
   });
 
   test('grab "Child 1" to the nested container', async ({ page }, testInfo) => {
