0a2a086d11fa1d417c4b955d8dd7e119efc1e514
Koala Yeung <koalay@gmail.com>
2023-11-12 23:02:32 +0800

    test: drag-drop the nested container parent in test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -65,12 +65,13 @@ describe('Simple drag drop', () => {
     // Identify the source and target containers.
     // Check that the source has the text "Child 1" and the target doesn't.
     const source = page.locator('.grid > dragdrop-container').first();
-    const target = page.locator('.grid > dragdrop-container > dragdrop-child > dragdrop-container').first();
+    const nestedContainer = page.locator('.grid > dragdrop-container > dragdrop-child > dragdrop-container').first();
+    const destination = page.locator('.grid > dragdrop-container').last();
     await expect(source).toContainText('Child 1');
-    await expect(target).not.toContainText('Child 1');
+    await expect(nestedContainer).not.toContainText('Child 1');
     await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
 
-    // Drag source to target.
+    // Drag first child from source to target.
     const child = page.locator('dragdrop-child').nth(0);
     expect(child).not.toBeNull();
     await child.hover();
@@ -83,10 +84,20 @@ describe('Simple drag drop', () => {
 
     // Drop on the target.
     // Check the drop is successful.
-    await target.hover();
+    await nestedContainer.hover();
     await page.mouse.up();
-    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop.png` });
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop1.png` });
     await expect(source).not.toContainText('Child 1');
-    await expect(target).toContainText('Child 1');
+    await expect(nestedContainer).toContainText('Child 1');
+
+    // Drag the parent of the nested container to the destination.
+    await page.mouse.move(231, 54); // handle location of the child that has container.
+    await page.mouse.down();
+    await destination.hover();
+    await page.mouse.up(); // drop on the destination
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop2.png` });
+    await expect(source).not.toContainText('Child 1');
+    await expect(nestedContainer).toContainText('Child 1');
+    await expect(destination).toContainText('Child 1');
   });
 });
