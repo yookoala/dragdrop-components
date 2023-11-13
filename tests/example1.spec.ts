4f65628e23301fad61e4532325d798d58432ab7a
Koala Yeung <koalay@gmail.com>
2023-11-13 16:39:33 +0800

    tests: extend coverage
    * Cover the re-order logic.
    * Improve coverage in reordering involves nested container.

--- tests/example1.spec.ts
+++ tests/example1.spec.ts
@@ -57,7 +57,85 @@ describe('Simple drag drop', () => {
     await expect(destination).toContainText('Child 1');
   });
 
-  test('grab "Child 1" to the nested container', async ({ page }, testInfo) => {
+  test('grab and reorder child before another child', async ({ page }, testInfo) => {
+    let step = 0;
+
+    // Identify the source and target containers.
+    // Check that the source has the text "Child 1" and the target doesn't.
+    const source = page.locator('.grid > dragdrop-container').first();
+    const nestedContainer = page.locator('.grid > dragdrop-container > dragdrop-child > dragdrop-container').first();
+    const destination = page.locator('.grid > dragdrop-container').last();
+    await expect(source).toContainText('Child 1');
+    await expect(nestedContainer).not.toContainText('Child 1');
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
+
+    // Identify child to use in the test.
+    const child1 = page.locator('dragdrop-child').nth(0);
+    expect(child1).not.toBeNull();
+
+    // Drag first child from source to destination.
+    await child1.hover();
+    await page.mouse.down();
+    await destination.hover();
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-moved-child1.png` });
+
+    // Drag second child and place it before the first child.
+    const child2 = page.locator('dragdrop-child').nth(0);
+    expect(child2).not.toBeNull();
+    await child2.hover();
+    await page.mouse.down();
+    await page.mouse.move(636, 86);
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-moved-child2.png` });
+
+    const text = await destination.innerText();
+    expect(text).toBe('Child 2\nChild 1');
+  });
+
+  test('grab and reorder child after another child', async ({ page }, testInfo) => {
+    let step = 0;
+
+    // Identify the source and target containers.
+    // Check that the source has the text "Child 1" and the target doesn't.
+    const source = page.locator('.grid > dragdrop-container').first();
+    const nestedContainer = page.locator('.grid > dragdrop-container > dragdrop-child > dragdrop-container').first();
+    const destination = page.locator('.grid > dragdrop-container').last();
+    await expect(source).toContainText('Child 1');
+    await expect(nestedContainer).not.toContainText('Child 1');
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
+
+    // Identify child to use in the test.
+    const child1 = page.locator('dragdrop-child').nth(0);
+    expect(child1).not.toBeNull();
+
+    // Drag first child from source to destination.
+    await child1.hover();
+    await page.mouse.down();
+    await destination.hover();
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-moved-child1.png` });
+
+    // Drag second child and place it before the first child.
+    const child2 = page.locator('dragdrop-child').nth(0);
+    expect(child2).not.toBeNull();
+    await child2.hover();
+    await page.mouse.down();
+    await page.mouse.move(636, 112);
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-moved-child2.png` });
+
+    const text = await destination.innerText();
+    expect(text).toBe('Child 1\nChild 2');
+  });
+});
+
+describe('Nested drag drop', () => {
+  test.describe.configure({
+    retries: 2,
+  });
+
+  test('move nested container-child', async ({ page }, testInfo) => {
     let step = 0;
 
     // Identify the source and target containers.
@@ -97,5 +175,157 @@ describe('Simple drag drop', () => {
     await expect(source).not.toContainText('Child 1');
     await expect(nestedContainer).toContainText('Child 1');
     await expect(destination).toContainText('Child 1');
+  });
+
+  test('reorder before nested container', async ({ page }, testInfo) => {
+    let step = 0;
+
+    // Identify the source and target containers.
+    // Check that the source has the text "Child 1" and the target doesn't.
+    const source = page.locator('.grid > dragdrop-container').first();
+    const nestedContainer = page.locator('.grid > dragdrop-container:nth-child(2) > dragdrop-child > dragdrop-container').first();
+    await expect(source).toContainText('Child 1');
+    await expect(nestedContainer).not.toContainText('Child 1');
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
+
+    // Drag first child from source into the nested container.
+    const child1 = page.locator('dragdrop-child').nth(0);
+    expect(child1).not.toBeNull();
+    await child1.hover();
+    await page.mouse.down();
+    await nestedContainer.hover();
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop1.png` });
+    await expect(source).not.toContainText('Child 1');
+    await expect(nestedContainer).toContainText('Child 1');
+
+    // Drag second child before the nested container.
+    const child2 = page.locator('dragdrop-child').nth(0);
+    expect(child2).not.toBeNull();
+    await child2.hover();
+    await page.mouse.down();
+    await page.mouse.move(274, 74);
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop2.png` });
+    await expect(source).not.toContainText('Child 2');
+    // Child 2 should be before nested container, not in it.
+    await expect(nestedContainer).not.toContainText('Child 2');
+
+    // Child 2 should be before Child 1
+    expect(page.locator('.grid > dragdrop-container:nth-child(2) dragdrop-child').first()).toContainText('Child 2');
+    expect(page.locator('.grid > dragdrop-container:nth-child(2) dragdrop-child').last()).toContainText('Child 1');
+  });
+
+  test('reorder after nested container', async ({ page }, testInfo) => {
+    let step = 0;
+
+    // Identify the source and target containers.
+    // Check that the source has the text "Child 1" and the target doesn't.
+    const source = page.locator('.grid > dragdrop-container').first();
+    const nestedContainer = page.locator('.grid > dragdrop-container:nth-child(2) > dragdrop-child > dragdrop-container').first();
+    await expect(source).toContainText('Child 1');
+    await expect(nestedContainer).not.toContainText('Child 1');
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
+
+    // Drag first child from source into the nested container.
+    const child1 = page.locator('dragdrop-child').nth(0);
+    expect(child1).not.toBeNull();
+    await child1.hover();
+    await page.mouse.down();
+    await nestedContainer.hover();
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop1.png` });
+    await expect(source).not.toContainText('Child 1');
+    await expect(nestedContainer).toContainText('Child 1');
+
+    // Drag second child before the nested container.
+    const child2 = page.locator('dragdrop-child').nth(0);
+    expect(child2).not.toBeNull();
+    await child2.hover();
+    await page.mouse.down();
+    await page.mouse.move(274, 280); // Should be after the nested container.
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop2.png` });
+    await expect(source).not.toContainText('Child 2');
+    // Child 2 should be after nested container, not in it.
+    await expect(nestedContainer).not.toContainText('Child 2');
+
+    // Child 2 should be after Child 1
+    expect(page.locator('.grid > dragdrop-container:nth-child(2) dragdrop-child').first()).toContainText('Child 1');
+    expect(page.locator('.grid > dragdrop-container:nth-child(2) dragdrop-child').last()).toContainText('Child 2');
+  });
+
+  test('reorder inside nested container before child 1', async ({ page }, testInfo) => {
+    let step = 0;
+
+    // Identify the source and target containers.
+    // Check that the source has the text "Child 1" and the target doesn't.
+    const source = page.locator('.grid > dragdrop-container').first();
+    const nestedContainer = page.locator('.grid > dragdrop-container:nth-child(2) > dragdrop-child > dragdrop-container').first();
+    await expect(source).toContainText('Child 1');
+    await expect(nestedContainer).not.toContainText('Child 1');
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
+
+    // Drag first child from source into the nested container.
+    const child1 = page.locator('dragdrop-child').nth(0);
+    expect(child1).not.toBeNull();
+    await child1.hover();
+    await page.mouse.down();
+    await nestedContainer.hover();
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop1.png` });
+    await expect(source).not.toContainText('Child 1');
+    await expect(nestedContainer).toContainText('Child 1');
+
+    // Drag second child before the nested container.
+    const child2 = page.locator('dragdrop-child').nth(0);
+    expect(child2).not.toBeNull();
+    await child2.hover();
+    await page.mouse.down();
+    await page.mouse.move(274, 138);
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop2.png` });
+    await expect(source).not.toContainText('Child 2');
+    await expect(nestedContainer).toContainText('Child 2');
+
+    const text = await nestedContainer.innerText();
+    expect(text).toBe('Child 2\nChild 1');
+  });
+
+  test('reorder inside nested container after child 1', async ({ page }, testInfo) => {
+    let step = 0;
+
+    // Identify the source and target containers.
+    // Check that the source has the text "Child 1" and the target doesn't.
+    const source = page.locator('.grid > dragdrop-container').first();
+    const nestedContainer = page.locator('.grid > dragdrop-container:nth-child(2) > dragdrop-child > dragdrop-container').first();
+    await expect(source).toContainText('Child 1');
+    await expect(nestedContainer).not.toContainText('Child 1');
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-setup.png` });
+
+    // Drag first child from source into the nested container.
+    const child1 = page.locator('dragdrop-child').nth(0);
+    expect(child1).not.toBeNull();
+    await child1.hover();
+    await page.mouse.down();
+    await nestedContainer.hover();
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop1.png` });
+    await expect(source).not.toContainText('Child 1');
+    await expect(nestedContainer).toContainText('Child 1');
+
+    // Drag second child before the nested container.
+    const child2 = page.locator('dragdrop-child').nth(0);
+    expect(child2).not.toBeNull();
+    await child2.hover();
+    await page.mouse.down();
+    await page.mouse.move(274, 181);
+    await page.mouse.up();
+    await page.screenshot({ path: `${testInfo.outputPath()}/${step++}-after-drop2.png` });
+    await expect(source).not.toContainText('Child 2');
+    await expect(nestedContainer).toContainText('Child 2');
+
+    const text = await nestedContainer.innerText();
+    expect(text).toBe('Child 1\nChild 2');
   });
 });
